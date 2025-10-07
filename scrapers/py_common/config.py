7d18ed7d3b0acd964491650c42fee8fbd8f9ce7d
Marqs <markus.rohsto@gmail.com>
2025-10-07 08:16:11 +0200

    kaka

--- scrapers/py_common/config.py
+++ scrapers/py_common/config.py
@@ -3,5 +3,5 @@ STASH = {
     "url":
         "http://localhost:9999",
     "api_key":
-        ""
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJlYjAzMzRkNi03NTQ4LTRhYjAtYjExMC0xOGEyZmI2Y2YwMDQiLCJzdWIiOiJBUElLZXkiLCJpYXQiOjE2NjM0NDg1MjZ9.ckvz_oNpI_HSCGSSOa1xI2mprnoDCBl7EuoBAXcK6Us"
 }
